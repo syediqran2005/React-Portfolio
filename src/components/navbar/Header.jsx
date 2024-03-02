7f1402f454dde01b3d52ba048d1af0bfee005b57
syediqran2005 <syediqran2004@gmail.com>
2024-03-02 22:49:42 +0500

    navbar responsiveness completed

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,59 +1,30 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './header.css'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { Container, Button , Nav, } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./header.css";
 
 function Header() {
   return (
     <>
-    <Navbar className="bg-body-tertiary navbar">
+ <Navbar bg="body-tertiary" expand="lg">
       <Container>
-        <Navbar.Brand style={{
-          color : 'white',
-          fontFamily : 'sans-serif',
-          fontWeight : 'bold',
-          fontSize : '1.5rem'
-        }} >JANELLE BROWN</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Text style={{
-          color : 'white',
-          marginLeft : '5%',
-          fontSize : '1rem',
-          fontWeight : 'bolder',
-          letterSpacing : '1px',
-          borderBottom : '3px solid white',
-          borderBottomRightRadius : '3px',
-          borderBottomLeftRadius : '3px'
-        }}>
-          ABOUT
-        </Navbar.Text>
-        <Navbar.Text style={{
-          color : 'white',
-          marginLeft : '5%',
-          fontSize : '1rem',
-          fontWeight : 'bolder',
-          letterSpacing : '1px',
-          
-          
-        }} className='workStyle'>
-          WORK
-        </Navbar.Text>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text style={{
-          color : 'white',
-          fontSize : '1rem',
-          fontWeight : 'bolder',
-          letterSpacing : '1px',
-        }}>
-          LET'S CONNECT <Button variant="light" className='pt-2 pb-2 mt-3 mb-3'  style={{borderRadius : '50px', width: '16rem' , fontSize : '18px'}} >syediqran2004@gmail.com</Button>
-          </Navbar.Text>
+        <Navbar.Brand className="navbarTitle">JANELLE BROWN</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{backgroundColor : 'grey'}} />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="nav1">
+            <Nav.Link className="aboutStyle">ABOUT</Nav.Link>
+            <Nav.Link className="workStyle">WORK</Nav.Link>
+          </Nav>
+          <Nav className="nav2">
+            <Nav.Item className="connectStyle mt-4" >LET'S CONNECT</Nav.Item>
+            <Button variant="light" className="pt-2 pb-2 mt-3 mb-3 emailStyle">syediqran2004@gmail.com</Button>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
